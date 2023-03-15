d387d43b0ba7288cc270d1073cb69ae7da899784
jimsaharov <jimps@inbox.ru>
2023-03-15 20:19:29 +0300

    добавлена отправка формы в ТГ

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ import JsonData from "./data/data.json";
 import { Img } from "./components/img";
 import { ModalDescription } from "./components/ModalDescription";
 import { About } from "./components/About";
-import { ChooseSup } from "./components/ChooseSup";
 import { OrderPage } from "./components/OrderPage";
 import { TelegramIcon } from "./components/TelegramIcon";
 import { Footer } from "./components/Footer";
 import { HowChooseSupModal } from "./components/HowChooseSupModal";
-
+import { GladiatorHeader } from "./components/GladiatorHeader";
 
 function App() {
   const [pageData, setPageData] = useState({});
@@ -26,22 +25,27 @@ function App() {
   const [showOrderPage, setShowOrderPage] = useState(false);
   const [showHowCooseSupModal, setShowHowCooseSupModal] = useState(false);
 
-  
-
   return (
     <div className="App">
       <Navigation />
-      <Header showHowCooseSupModal={showHowCooseSupModal} setShowHowCooseSupModal={setShowHowCooseSupModal} />
-      <TelegramIcon/>
+      <Header
+        showHowCooseSupModal={showHowCooseSupModal}
+        setShowHowCooseSupModal={setShowHowCooseSupModal}
+      />
+      <TelegramIcon />
       <About />
-      <ChooseSup />
-      
-      <OrderPage showOrderPage={showOrderPage} setShowOrderPage={setShowOrderPage} selectItem={selectItem}/>
+      <GladiatorHeader/>
+
+      <OrderPage
+        showOrderPage={showOrderPage}
+        setShowOrderPage={setShowOrderPage}
+        selectItem={selectItem}
+        setSelectItem={setSelectItem}
+      />
       <Catalog
         setSelectItem={setSelectItem}
         data={pageData.Gladiator}
         setShowModal={setShowModal}
-      
       />
       <Img />
       <Catalog
@@ -56,9 +60,18 @@ function App() {
         showModal={showModal}
         setShowModal={setShowModal}
       />
-      <HowChooseSupModal showHowCooseSupModal={showHowCooseSupModal} setShowHowCooseSupModal={setShowHowCooseSupModal}/>
-      <iframe title="map" src="https://yandex.ru/map-widget/v1/?um=constructor%3Aae787bd5cbfc82facc29f66c57d0d39adfbee5bf959358c554664e479115eeb6&amp;source=constructor" width="1054" height="565" frameborder="0"></iframe>
-      <Footer/>
+      <HowChooseSupModal
+        showHowCooseSupModal={showHowCooseSupModal}
+        setShowHowCooseSupModal={setShowHowCooseSupModal}
+      />
+      {/* <iframe
+        title="map"
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3Aae787bd5cbfc82facc29f66c57d0d39adfbee5bf959358c554664e479115eeb6&amp;source=constructor"
+        width="1054"
+        height="565"
+        frameborder="0"
+      ></iframe> */}
+      <Footer />
     </div>
   );
 }
--- src/components/OrderPage.jsx
+++ src/components/OrderPage.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { Image } from "react-bootstrap";
+import { useState } from "react";
 
 const styles = {
   container: {
     width: "100%",
     margin: "0 auto",
-    padding: "0px 30px"
+    padding: "0px 30px",
   },
   input: {
     width: "100%",
@@ -29,21 +30,40 @@ const styles = {
     borderBottom: "1px solid lightgrey",
     padding: "0px 30px",
   },
-  order_p:{
+  order_p: {
     marginBottom: "2px",
     fontWeight: "bold",
   },
-  order_description_text:{
+  order_description_text: {
     marginLeft: "40px",
   },
-  order_price_section:{
+  order_price_section: {
     display: "flex",
     justifyContent: "flex-start",
     padding: "10px 30px",
-  }
+  },
 };
 
 export function OrderPage(props) {
+  
+  const [name, setName] = useState("");
+  const [tel, setTel] = useState("");
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    
+    fetch(
+      `https://api.telegram.org/bot5916265051:AAElzZRUOTI-1cH7OqYcaqQyINKCdGfPAIA/sendMessage?chat_id=-1001837804274&text=${name}%20${tel}%20${props.selectItem.model}%20${props.selectItem.price}`,
+      {
+        method: "POST",
+        
+      }
+    )
+      props.setShowOrderPage(false)
+  };
+
   return (
     <Modal
       show={props.showOrderPage}
@@ -63,34 +83,63 @@ export function OrderPage(props) {
           ></Image>
 
           <div style={styles.order_description_text}>
-            <p style={styles.order_p}> {props.selectItem ? props.selectItem.model : "load"}</p>
+            <p style={styles.order_p}>
+              {" "}
+              {props.selectItem ? props.selectItem.model : "load"}
+            </p>
             <ul>
               <li>{props.selectItem ? props.selectItem.param1 : "load"}</li>
               <li>{props.selectItem ? props.selectItem.param2 : "load"}</li>
               <li>{props.selectItem ? props.selectItem.size : "load"}</li>
             </ul>
           </div>
-          <div>
-            
-          </div>
+          <div></div>
         </div>
         <div style={styles.order_price_section}>
-        <span>Сумма заказа:</span> <span  style={styles.order_p}> {props.selectItem ? props.selectItem.price : "load"}</span>
+          <span>Сумма заказа:</span>{" "}
+          <span style={styles.order_p}>
+            {" "}
+            {props.selectItem ? props.selectItem.price : "load"}
+          </span>
         </div>
-        <div style={styles.order_price_section}><p>Заполните и отправьте форму. В ближайшее время наши менеджеры свяжутся с Вами. </p></div>
-        
+        <div style={styles.order_price_section}>
+          <p>
+            Заполните и отправьте форму. В ближайшее время наши менеджеры
+            свяжутся с Вами.{" "}
+          </p>
+        </div>
+
         <div style={styles.container}>
-          <form>
-            <input placeholder="Имя" style={styles.input} value={props.selectItem ? props.selectItem.model : "load"} />
-            <input placeholder="E-mail" style={styles.input} />
-            <input placeholder="Тел +7(000) 000 00 00" style={styles.input} />
-            
-            <Button variant="warning"  style={styles.order_form_button} type="submit">
+          <form onSubmit={handleSubmit}>
+            <input
+              readOnly
+              style={styles.input}
+              value={props.selectItem ? props.selectItem.model : "load"}
+            />
+            <input
+              placeholder="Имя"
+              style={styles.input}
+              name="name"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              placeholder="Тел +7(000) 000 00 00"
+              style={styles.input}
+              name="tel"
+              type="text"
+              onChange={(e) => setTel(e.target.value)}
+            />
+
+            <Button
+              variant="warning"
+              style={styles.order_form_button}
+              type="submit"
+            >
               Отправить
             </Button>
           </form>
         </div>
-        
       </Modal.Body>
     </Modal>
   );
